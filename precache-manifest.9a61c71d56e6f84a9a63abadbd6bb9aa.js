8685c573b3508838a156a81d4ff8609dac4b3325
quarlesh <quarlesh@msu.edu>
2020-04-21 00:45:01 -0400

    changes

---
+++ precache-manifest.9a61c71d56e6f84a9a63abadbd6bb9aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4195a87bfb4aabbcadbece1fef841203",
+    "url": "/text-analyzer-ui/index.html"
+  },
+  {
+    "revision": "2bb8a103dd1b659a9484",
+    "url": "/text-analyzer-ui/static/js/2.de34f3c2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/text-analyzer-ui/static/js/2.de34f3c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ff5e3368326ec261e26",
+    "url": "/text-analyzer-ui/static/js/main.e77a55ae.chunk.js"
+  },
+  {
+    "revision": "38363184ee0712865be8",
+    "url": "/text-analyzer-ui/static/js/runtime-main.15ffa85c.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/text-analyzer-ui/precache-manifest.9a61c71d56e6f84a9a63abadbd6bb9aa.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/text-analyzer-ui/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
